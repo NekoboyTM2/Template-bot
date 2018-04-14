526da51cc567799a836d2f6619d9d20fc3ee495c
GitStonic <kingstonicthe1@gmail.com>
2018-04-14 15:56:29 -0400

    Fixed some things and removed what's not needed...

--- bot.js
+++ bot.js
@@ -1,28 +1,24 @@
-const { Client, Util } = require('discord.js');
-const Discord = require('discord.js');
+const { Client, Collection } = require('discord.js');
+const { TOKEN, PREFIX } = require('./config');
+const client = new Client();
 const fs = require('fs');
 
-const { TOKEN } = require('./config'); // make sure you added your bot's token there
+client.commands = new Collection();
+client.helpcommands = new Collection();
 
-const client = new Discord.Client();
+fs.readdir('./commands/', (err, files) => {
+	if (err) console.log(err);
+	let jsfile = files.filter(file => file.split('.').pop() === 'js');
+	if (jsfile.length <= 0) {
+		console.log("Couldn't find commands.");
+		return;
+	}
 
-client.commands = new Discord.Collection();
-client.helpcommands = new Discord.Collection();
-
-fs.readdir("./commands/", (err, files) => {
-
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js");
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
-
-  jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded`);
-    client.commands.set(props.help.name, props);
-  });
+	jsfile.forEach(file => {
+		let props = require(`./commands/${file}`);
+		console.log(`${file} loaded`);
+		client.commands.set(props.help.name, props);
+	});
 });
 
 client.on('warn', console.warn);
@@ -31,45 +27,41 @@ client.on('error', console.error);
 
 client.on('ready', () => console.log(`${client.user.username} is onlne`));
 
-  client.on('ready', () => {
-	client.user.setActivity(`smth gud ig`, { type: "PLAYING" }); //you can edit this to whatever you want, but remember the type can only be "PLAYING", "WATCHING", "LISTENING", or "STREAMING"
-});
+client.on('ready', () => client.user.setActivity(`smth gud ig`, { type: 'PLAYING' })); // you can edit this to whatever you want, but remember the type can only be "PLAYING", "WATCHING", "LISTENING", or "STREAMING"
 
 client.on('disconnect', () => console.log('I just disconnected, making sure you know'));
 
 client.on('reconnecting', () => console.log('I will reconnect now...'));
 
-client.on('message', async message => { 
+client.on('message', message => {
 	if (message.author.bot) return undefined;
-	if (message.content.startsWith("<your default prefix>") && message.channel.type !== "text")return mesage.reply("Please use my commands in a server");
-								//just edit ^^this^^ to your default prefix
-								
-	let prefixes = JSON.parse(fs.readFileSync("./jsons/prefixes.json", "utf8"));
-    if(!prefixes[message.guild.id]){
-    prefixes[message.guild.id] = {
-      prefixes: "<a-prefix>"  //this is the default prefix (you can set it to whatever you want)
-    };
-  }
-   const prefix = prefixes[message.guild.id].prefixes;
- 
- // i just added the ping command here instead of making another file in ./commands
-  if (message.content == `${prefix}ping`){
-	  message.channel.send('pong');
-			message.channel.send('Pinging...').then(sent => {
-    sent.edit(`my ping - ${sent.createdTimestamp - message.createdTimestamp}ms`);
-	console.log(`ping command has been used by ${message.author.username} in ${message.guild}`);
-    });
-  }
-  
-  if(!message.content.startsWith(prefix))return;
-  
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
+	if (!message.content.startsWith(PREFIX)) return;
+	if (message.content.startsWith(PREFIX) && message.channel.type !== 'text') return message.reply('Please use my commands in a server');
+	// just edit ^^this^^ to your default prefix
 
-  let commandfile = client.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(client,message,args);
-  
+	let prefixes = JSON.parse(fs.readFileSync('./jsons/prefixes.json', 'utf8'));
+
+	if (!prefixes[message.guild.id]) prefixes[message.guild.id] = { prefixes: PREFIX };
+
+	const prefix = prefixes[message.guild.id].prefixes;
+
+	// i just added the ping command here instead of making another file in ./commands
+	if (message.content === `${prefix}ping`) {
+		message.channel.send('pong');
+		message.channel.send('Pinging...').then(sent => {
+			sent.edit(`my ping - ${sent.createdTimestamp - message.createdTimestamp}ms`);
+			console.log(`ping command has been used by ${message.author.username} in ${message.guild}`);
+		});
+	}
+
+	if (!message.content.startsWith(prefix)) return;
+
+	let messageArray = message.content.split(' ');
+	let cmd = messageArray[0];
+	let args = messageArray.slice(1);
+
+	let commandfile = client.commands.get(cmd.slice(prefix.length));
+	if (commandfile) commandfile.run(client, message, args);
 });
 
 client.login(TOKEN);
